eea49bfe315dbf8978f5f5117abe2f4c7fb06d29
forhyeonji <ghkdguswl3@gmail.com>
2023-02-10 18:08:19 +0900

    generic

---
+++ 5-generic/5-3-constrains.ts
@@ -0,0 +1,39 @@
+{
+  /**
+   * generic에 조건주기
+   */
+
+  interface Employee {
+    pay(): void;
+  }
+
+  class FullTimeEmployee implements Employee {
+    pay(): void {
+      console.log(`full time!!!`);
+    }
+    workFullTime() {}
+  }
+
+  class PartTimeEmployee implements Employee {
+    pay(): void {
+      console.log(`Part time!!!`);
+    }
+    workPartTime() {}
+  }
+
+  // 세부적인 타입을 인자로 받아서 정말 추상적인 타입으로 다시 리턴하는 함수는 💩💩💩
+  function payBad(employee: Employee): Employee {
+    employee.pay();
+    return employee;
+  }
+
+  function pay<T extends Employee>(employee: T): T {
+    employee.pay();
+    return employee;
+  }
+
+  // ✨✨✨
+  function getValue<T, K extends keyof T>(obj: T, key: K): T[K] {
+    return obj[key];
+  }
+}
